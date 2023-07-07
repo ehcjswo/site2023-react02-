66cad0907dd3b58706da3c8f6aa1a52b662fc074
KimDoHyeon <ehcjswo1@gmail.com>
2023-07-07 10:44:32 +0900

    07-07

--- src/components/section/MyInfo.jsx
+++ src/components/section/MyInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MyInfo = (props) => {
   return (
@@ -7,7 +8,7 @@ const MyInfo = (props) => {
             <h2><span>김도현님</span>의 성장 여정이에요.</h2>
         </div>
         <div className="myInfo_box">
-            <a href="#">
+            <Link to="#">
                 <div className="myInfo_box_shild">
                     <div className="myInfo_box2">
                         <p>정복중인 레벨</p>
@@ -17,8 +18,8 @@ const MyInfo = (props) => {
                     </div>
                 </div> 
                 <img className="AegzswwsBVc8X7_R6mHo" src="https://d1nuzc1w51n1es.cloudfront.net/410a322070ecd6a31b2c.png" alt="정복중인 레벨 아이콘" />
-            </a>
-            <a href="#">
+            </Link>
+            <Link to="#">
                 <div className="myInfo_box_shild">
                     <div className="myInfo_box2">
                         <p>현재 랭킹</p>
@@ -28,8 +29,8 @@ const MyInfo = (props) => {
                     </div>
                 </div> 
                 <img className="AegzswwsBVc8X7_R6mHo" src="https://d1nuzc1w51n1es.cloudfront.net/87233c1b6171f4b6fdfa.png" alt="현재 랭킹 아이콘" />
-            </a>
-            <a href="#">
+            </Link>
+            <Link to="#">
                 <div className="myInfo_box_shild">
                     <div className="myInfo_box2">
                         <p>해결한 코딩테스트</p>
@@ -39,8 +40,8 @@ const MyInfo = (props) => {
                     </div>
                 </div> 
                 <img className="AegzswwsBVc8X7_R6mHo" src="https://d1nuzc1w51n1es.cloudfront.net/c74175c3bd1106944db4.png" alt="해결한 코딩테스트 아이콘" />
-            </a>
-            <a href="#">
+            </Link>
+            <Link to="#">
                 <div className="myInfo_box_shild">
                     <div className="myInfo_box2">
                         <p>채용 현황 관리</p>
@@ -50,7 +51,7 @@ const MyInfo = (props) => {
                     </div>
                 </div> 
                 <img className="AegzswwsBVc8X7_R6mHo" src="https://d1nuzc1w51n1es.cloudfront.net/b4786c86ab1e07b15d63.png" alt="채용 현황 관리 아이콘" />
-            </a>
+            </Link>
         </div>
     </div>
   )
